06fcc6e896759433fa09d54e73b03c7b7b11574a
Reena314 <reenadeveloper314@gmail.com>
2025-02-12 11:40:33 +0000

    variables

---
+++ Basics/variables.js
@@ -0,0 +1,15 @@
+const empId = 301
+let empEmail = "rahul@google.com"
+var empPwd = "3412"
+empCity = "jaipur"
+ 
+//empId = 401   // not allowed
+empEmail = "riya@google.com"
+empPwd = "erty"
+empCity = "mp"
+let empState;   // undefined
+/* prefer not to use var
+ because of issue in block scope and functional scope
+ */
+console.log(empId);
+console.table([empEmail,empPwd,empCity, empState]);
