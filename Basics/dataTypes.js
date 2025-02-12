7532856a313ca1fb0a3351b97ae0f3d40f84e63f
Reena314 <reenadeveloper314@gmail.com>
2025-02-12 12:15:32 +0000

    datatypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use Strict" // treat all js code as newer version
+
+//alert(2 + 2) // we are using nodejs, not browser
+console.log(2 + 2);
+
+let name = "Riya"
+let age = 30
+let isLoggedIn = false
+let state;
+
+// PRIMITIVE DATA TYPE:-
+// number => 2 to power 53
+// bigInt
+// string => true/false
+// null =>standalone value
+// undefined
+// symbol => unique
+
+// OBJECT
+
+
+console.log(typeof 'riya')
+console.log(typeof age)
+console.log(typeof null)   // null is object 
+console.log(typeof undefined) // undefined (its type)
