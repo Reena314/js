5c8e4a1a3c9d71f30fd8ed1194098e6bc17899df
Reena Kumari <reenadeveloper314@gmail.com>
2025-04-09 12:55:11 +0000

    dateandtime

---
+++ Basics/09_dateandtime.js
@@ -0,0 +1,15 @@
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+let newCreate = new Date(2025, 0, 9, 6, 25);
+let newCreate = new Date(2025, 0, 9, 6, 25);
+console.log(newCreate.toLocaleString());
+
+
