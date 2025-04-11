dc49f3aee0c5b45be41e8d412e61804589f9b126
Reena Kumari <reenadeveloper314@gmail.com>
2025-04-11 11:27:19 +0000

    function

---
+++ 02_Basics/05_function.js
@@ -0,0 +1,34 @@
+function user(){
+    console.log("r")
+    console.log("i")
+    console.log("y")
+    console.log("a")
+}
+user()
+//
+
+const MyFunction = function(){
+    //console.log("hello");      //  show undefined
+    return 'hello';
+    
+}
+ console.log(MyFunction());
+
+ //
+//  function addnumbers(num1 , num2){
+//     console.log(num1+num2);
+//  }
+//  addnumbers(3, 5)
+
+
+function addnumbers(num1 , num2){
+    return (num1+num2);
+    console.log(result);       // after return not work 
+ }
+ const result = addnumbers(3, 5)
+
+ console.log(result);
+ 
+ 
+ 
+ 
