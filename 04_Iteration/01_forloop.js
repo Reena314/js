337a592f95c55d9fbd1274c82ed3230d96387f38
Reena Kumari <reenadeveloper314@gmail.com>
2025-04-15 12:56:44 +0000

    for loop

---
+++ 04_Iteration/01_forloop.js
@@ -0,0 +1,64 @@
+
+// for (let index = 0; index < 10; index++){
+//     const element = index;
+//     console.log(element);
+// }
+    
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+//     if (index == 5) {
+//         console.log('5 is a best num');
+        
+//     }
+    
+// }
+// console.log('default');
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+for (let i = 0; i < 10; i++) {
+    console.log(`outer value ${i}`);
+    for (let j = 0; j < 10; j++) {
+       // console.log(`inner value ${j} and inner loop ${i}`);
+        console.log(i + '*' + j + '=' + i*j );
+        
+        
+    }
+    
+}
+// ++++++++++++++++++++++++++++Array+++++++++++++++++++++++
+  let
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+ // +++++++++++++++++++++++++++++++++++++++++break and continue++++++++++++
+
+//  for (let index = 1; index < 20; index++) {
+
+//     if (index == 5) {
+//         console.log('5best num');
+//         break;
+        
+//     }
+   
+//     console.log(index);
+    
+    
+//  }
+
+
+
+// for (let index = 1; index < 20; index++) {
+
+//     if (index == 5) {
+//         console.log('5best num');
+//         continue;
+        
+//     }
+   
+//     console.log(index);
+    
+    
+//  }
