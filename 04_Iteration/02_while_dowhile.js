238267ef52d91ddbbd0e4bd956bc14cf86a61118
Reena Kumari <reenadeveloper314@gmail.com>
2025-04-16 11:04:47 +0000

    while do while loop

---
+++ 04_Iteration/02_while_dowhile.js
@@ -0,0 +1,28 @@
+// let a = 0;
+// while(a<= 10){
+//     console.log(`value is ${a}`);
+//     a = a+1;
+    
+// }
+
+
+// let newArray = ["flexseed", "chia seed", "pumkin seed"]
+// let arr = 0;
+
+// while (arr<= newArray.length) {
+//     console.log(` value is ${newArray[arr]}`);
+//     arr = arr+1
+    
+// }
+
+
+
+//+++++++++++++++dowhile+++++++++++++++++++
+
+ let score = 1;
+
+do{
+  console.log('score is ', score);
+  score++;
+  
+}while (score<10) 
